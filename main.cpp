8d83327a0076af1592229a033f84db0354981edc
MystX Jones <mystx@chaostheory.co.nz>
2011-07-08 00:22:49 +1200

    Tidied everything, added debug output, added speed-modified camera

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 #define PI 3.14159265
 
 
+// Returns n pipes in a string
+// Hacks as, bro
 void getPipe(int n, char* s) {
     int i;
     for (i = 0; i < n; ++i) {
@@ -23,6 +25,8 @@ void getPipe(int n, char* s) {
     s[i] = NULL;
 }
 
+// Sets up the graphics environment
+// TODO: Lighting, anti-aliasing and fog
 void setupGraphics()
 {
     //Attemp at getting anti-aliasing to work
@@ -34,7 +38,7 @@ void setupGraphics()
 
 
     //Attempt at getting lighting to work
-    glEnable(GL_LIGHTING);
+    /*glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
     glEnable(GL_COLOR_MATERIAL);
@@ -54,7 +58,7 @@ void setupGraphics()
     glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, al);
     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, light_specular);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, light_emission);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, light_emission);*/
 
 
     //Attempt at getting fog to work
@@ -82,38 +86,41 @@ void setupGraphics()
     gluPerspective(75.f, 1.f, 1.f, 1000.f); //This needs to be set to a shorter value after track preview
 }
 
-
+// Main method 
+// Should the main game loop  / operations be in it's own class to save this for arg parsing and stuff?
 int main(int argc, char* argv[])
 {
+	int TRACK_LENGTH = 50; //Number of chunks to create
     state = 0; //Startup
 
     std::locale loc;
-    const std::collate<char>& coll = std::use_facet<std::collate<char> >(loc); // Used for hashing
+    const std::collate<char>& coll = std::use_facet<std::collate<char> >(loc); // Copypasta used for hashing
 
     //Parse commandline options
     for (int i = 0; i < argc; i++)
     {
-        std::cout << argv[i] << std::endl;
-        /*if (argv[i] == "--debug")
+        if (strcmp(argv[i], "--debug") == 0)
         {
+			std::cout << "DEBUG ENABLED: This could cause lower framerates than normal, and should NOT be used by default." << std::endl;
             debug = true;
-        }*/
+        }
         if (strcmp(argv[i], "-s") == 0)
         {
             i++;
             seed = coll.hash(argv[i], argv[i]+strlen(argv[i]));
+			if (debug) std::cout << "Seeding with " << seed << "..." << std::endl;
         }
     }
 
-    int TRACK_LENGTH = 50; //Number of chunks to create
-
-    if (seed == 0)
+    if (seed == 0) {
         seed = time(NULL);
+		if (debug) std::cout << "No seed given - using current time..." << std::endl;
+	}
     srand (seed);
 
     // Create the main window
-    sf::RenderWindow App(sf::VideoMode(1000, 1000, 24), "SFML window");
-    App.UseVerticalSync(true);
+    sf::RenderWindow App(sf::VideoMode(1000, 1000, 32), "Razor Wire");
+    //App.UseVerticalSync(true);
     App.PreserveOpenGLStates(true);
 
     sf::Clock Clock;
@@ -122,54 +129,52 @@ int main(int argc, char* argv[])
 
     // Initialise track
     Track track;
-    double time = Clock.GetElapsedTime();
-    track.GenChunk(TRACK_LENGTH);
-    time = Clock.GetElapsedTime() - time;
-    std::cout << "Level generated in " << time << " seconds." << std::endl;
+    if (debug)
+	{
+		double time = Clock.GetElapsedTime();
+		track.GenChunk(TRACK_LENGTH);
+		time = Clock.GetElapsedTime() - time;
+		std::cout << "Level generated in " << time << " seconds." << std::endl;
+		
+		std::cout << "Setting up track preview" << std::endl;
+	}
+	else
+	{
+		track.GenChunk(TRACK_LENGTH);
+	}
 
     Point trackAvg = track.GetAvg();
-    std::cout << "Track middle point: " << trackAvg << std::endl;
-
     float max = track.GetMaxDist(trackAvg);
-    std::cout << "Track max distance from center: " << max << std::endl;
 
     // Initialise player
     Player p(track.getStart());
 
     const sf::Input &in = App.GetInput();
+    char pipetext[80]; // hacks
+	
+    state = 2; // Preview
 
-    //hacks
-    char pipetext[80];
-    state = 2;
-
+	glMatrixMode(GL_MODELVIEW);
+	
 	// Start the game loop
     while (App.IsOpened())
     {
         if (state == 2)
         {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            glMatrixMode(GL_MODELVIEW);
-            glColor3f(1, 1, 1);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
+            glColor3f(1, 1, 1); // White
             glLoadIdentity();
-
-            //glPushMatrix();
-            //glPopMatrix();
-            //glRotatef(Clock.GetElapsedTime()*30, 1, 0, 0);
-            //glRotatef(Clock.GetElapsedTime()*50, 0, 1, 0);
-            //glLoadIdentity();
-            gluLookAt(max + 100, 0, 0, 0, 0, 0, 0, 0, 1);
-            glRotatef(Clock.GetElapsedTime()*30, 1, 0, 0);
+			
+            gluLookAt(max + 100, 0, 0, 0, 0, 0, 0, 0, 1); 			// Look at the origin, from a large x distance away
+            glRotatef(Clock.GetElapsedTime()*30, 1, 0, 0);		// Rotate on all 3 axis at different speeds
             glRotatef(Clock.GetElapsedTime()*40, 0, 1, 0);
             glRotatef(Clock.GetElapsedTime()*50, 0, 0, 1);
-            glTranslatef(-trackAvg.x, -trackAvg.y, -trackAvg.z);
+            glTranslatef(-trackAvg.x, -trackAvg.y, -trackAvg.z);	// Translate the avg track pos
 
-            track.Render(App.GetFrameTime(), 1.0);
-
-
-
-
+            track.Render(App.GetFrameTime(), 1.0); 				// Render the track all pretty and such
 
             App.Display();
+			
             // Process events
             sf::Event Event;
             while (App.GetEvent(Event))
@@ -179,7 +184,8 @@ int main(int argc, char* argv[])
                     (Event.Type == sf::Event::KeyPressed && Event.Key.Code == sf::Key::Escape))
                     App.Close();
 
-                if (Event.Type == sf::Event::KeyPressed && Event.Key.Code == sf::Key::Return)
+                // Start track
+				if (Event.Type == sf::Event::KeyPressed && Event.Key.Code == sf::Key::Return)
                     state = 3;
 
                 if (Event.Type == sf::Event::Resized)
@@ -187,13 +193,14 @@ int main(int argc, char* argv[])
             }
 
         }
-        else if (state == 3)
+        else if (state == 3) // Playing
         {
 
             // hack alert
             char s[80];
             getPipe(20 - p.trackDist(p.tp), pipetext);
-            sprintf(s, "%f\n%i\n%s", 1/App.GetFrameTime(), (int)p.score, pipetext);
+            if (debug)	sprintf(s, "%f\n%i\n%s", 1/App.GetFrameTime(), (int)p.score, pipetext); // Add a frame rate counter if we're in debug mode
+			else			sprintf(s, "%i\n%s", (int)p.score, pipetext);
             sf::String Text(s);
 
             Text.SetColor(sf::Color(255,255,255));
@@ -209,6 +216,10 @@ int main(int argc, char* argv[])
 
                 if (Event.Type == sf::Event::Resized)
                     glViewport(0, 0, Event.Size.Width, Event.Size.Height);
+				
+				// Pause
+				if (Event.Type == sf::Event::KeyPressed && Event.Key.Code == sf::Key::P)
+					state = 4;
             }
 
             if (in.IsKeyDown(p.controls[A])) {
@@ -238,75 +249,41 @@ int main(int argc, char* argv[])
             else {
                 p.rv.x = 0;
             }
-
-
-            //Set the camera to be just behind the player
-
-
-            /*cam.r.x = -pPos.x*10/len;
-            cam.r.y = -pPos.y*10/len;
-            cam.r.z = -pPos.z*10/len;
-            cam.rr.x = p.rr.x;
-            cam.rr.y = p.rr.y;
-            cam.rr.z = p.rr.z;*/
-
-            //p.v = p.toWorld(Point(0, -2, 0));
-            // Clear screen
-            //App.Clear();
-            //------------
-            // OPENGL
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            glMatrixMode(GL_MODELVIEW);
-            glLoadIdentity();
-
-            // CAMERA
-
-            //glRotatef(cam.rr.z, 0,0,1);
-            //glRotatef(cam.rr.y, 0,1,0);
-
-
-            //cam.r.x = p.r.x;
-            //cam.r.y = p.r.y - 10;
-            //Point pPos = p.GetVector();
-
-            //p.v.x = pPos.x * 5;
-            //p.v.y = pPos.y * 5;
-            //p.v.z = pPos.z * 5;
-            //Point pUp = p.GetUpVector();
-            //std::cout << pUp << std::endl;
-            //float len = sqrt(pPos.x*pPos.x + pPos.y*pPos.y + pPos.z*pPos.z);
-
-            float camx = p.r.x - p.forward.x*15 + p.up.x*15;
-            float camy = p.r.y - p.forward.y*15 + p.up.y*15;
-            float camz = p.r.z - p.forward.z*15 + p.up.z*15;
-            // EcksJay shitting about
-            //p.v = p.toWorld(Point(0,1,0));
-            //p.v = p.GetHeading();
-            //std::cout << p.v << std::endl;
-            //std::cout << p.r.x << " " <<  p.r.y << " " << p.r.z << std::endl;
-            //glRotatef(p.rr.x * 180/PI, 1, 0, 0);
-            //glRotatef(p.rr.y * 180/PI, 0, 1, 0);
-            //glRotatef(p.rr.z * 180/PI, 0, 0, 1);
-            float speed = 30 - p.trackDist(p.tp)*2;
+			
+			float speed = 30 - p.trackDist(p.tp)*2;
             if (speed < 2) {
                 speed = 2;
             }
+			
+            //------------
+            // OPENGL
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			
+            glLoadIdentity();
+
+            // CAMERA
+			
+			// Set the camer position based on the ship's forward vector
+			// Here incremental values could be added to produce a 'soft' camera movement
+			// Currently the camera gets further away as speed increases
+            float camx = p.r.x - p.forward.x*15 - p.forward.x*speed/5 + p.up.x*15; // No speed modifier on the 'up' term to make the camera be more behind the ship at high speed and less above it
+            float camy = p.r.y - p.forward.y*15 - p.forward.y*speed/5 + p.up.y*15;
+            float camz = p.r.z - p.forward.z*15 - p.forward.z*speed/5 + p.up.z*15;
+            
+			// Update the ships velocity based on the player's ability to not suck
             p.v.x = p.forward.x*speed;
             p.v.y = p.forward.y*speed;
             p.v.z = p.forward.z*speed;
-            //std::cout << p.forward << " " << p.v << std::endl;
-            //std::cout << "Distance from track: " << p.trackDist(p.tp) << std::endl;
 
             //DYNAMIC CAMERA
             gluLookAt(camx, camy, camz, p.r.x, p.r.y, p.r.z, p.up.x, p.up.y, p.up.z);
 
-            //STATIC CAMERA
+            //STATIC CAMERA - uncomment to have the camera still at the start of the track
             //glRotatef(-70, 1,0,0);
             //glTranslatef(0, 10, -5);
 
             // OBJECTS
-            // The line
+            // The line from ship to current track segment
             glLineWidth(1.0);
             float col = 1 - p.trackDist(p.tp)/10;
             glColor3f(col, col, 1);
@@ -316,18 +293,38 @@ int main(int argc, char* argv[])
                 glVertex3f(p.r.x, p.r.y, p.r.z);
             glEnd();
 
-            track.Render(App.GetFrameTime(), 0.2);
+            track.Render(App.GetFrameTime(), 0.2); // Render track and player
             p.Render(App.GetFrameTime());
 
-            //p.r.y = p.r.y + 0.02f;
-
-            App.Draw(Text);
+            App.Draw(Text);	// Draw HUD text
 
             //------------
 
             // Update the window
             App.Display();
         }
+	else if (state == 4) // Game is paused
+	{
+		sf::Event Event;
+		while (App.GetEvent(Event))
+		{
+			// Close window : exit
+			if ((Event.Type == sf::Event::Closed) |
+				(Event.Type == sf::Event::KeyPressed && Event.Key.Code == sf::Key::Escape))
+				App.Close();
+
+			// Unpause
+			if (Event.Type == sf::Event::KeyPressed && Event.Key.Code == sf::Key::P)
+			{
+				state = 3;
+			}
+
+			if (Event.Type == sf::Event::Resized)
+				glViewport(0, 0, Event.Size.Width, Event.Size.Height);
+		}
+		
+		App.Display();
+	}
     }
 
     return EXIT_SUCCESS;
